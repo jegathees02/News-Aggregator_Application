e5f7d9ebb1c6b4c1a45cfe163d989685486e4b87
jegathees02 <100774735+jegathees02@users.noreply.github.com>
2023-07-08 15:56:03 +0530

    day-1 updated

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -4,21 +4,39 @@ import "../components/css/login.css";
 function Login() {
     return(
         <>
-        <div className="login-main">
-            <div className="login-logo-section" >
+        <div className="login-main container">
+            <div className="login-logo-section row" >
                 <Logo/>
             </div>
-            <div className="login-content">
-                 <div className="login-img" >
-                    <img className="login-img-main" src="https://c1.wallpaperflare.com/preview/415/175/739/magnifier-newspaper-history-glass.jpg" alt="img" />
-                 </div>
-                 <div className="login-inputs" >
-                    <div className="login-input-main" >
-                        <div className="login-text">
-                            <h2 className="text-green-500">iNews. Login</h2>
+            <div className="login-content row">
+                 <div className="login-img col-md-12 col-sm-2 col-lg-12"  >
+                    <div>
+                        <img className="login-img-main" src="https://c1.wallpaperflare.com/preview/415/175/739/magnifier-newspaper-history-glass.jpg" alt="img" />
+                    </div>
+                </div>
+                    <div className="login-inputs" >
+                        <div className="login-input-main" >
+                            <div className="login-text">
+                                <h2 className="text-slate-50 text-2xl font-bold text-center">iNews. Login</h2>
+                            </div>
+                            <div className="login-input-form">
+                                <form>
+                                    <label className="text-slate-100">Username/Email:
+                                        <input type="text" placeholder="Email/Username" />
+                                    </label>
+                                    <label className="text-slate-100">Password:
+                                        <input type="password" placeholder="Password"></input>
+                                    </label>
+                                    <label className="text-slate-100">
+                                        New User??
+                                        <a href="/signup" >SignUp</a>
+                                        {/* <Link to "/signup">Sign Up </Link> */}
+                                    </label>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                 </div>
+                
             </div>
         </div>
         </>
