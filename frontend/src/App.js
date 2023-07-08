32afed69464996fdc50d41b8863963322586ea34
jegathees02 <100774735+jegathees02@users.noreply.github.com>
2023-07-08 12:28:45 +0530

    configure_tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import getStartedComponent from './components/getStartedComponent';
+import Logo from './components/logo';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         {/* <Route path = "/" element = {<getStartedComponent /> } /> */}
         <Route path = "/" Component={getStartedComponent} />
+        <Route path = "/logo" Component={Logo} />
       </Routes>
       </Router>
     </div>
--- frontend/src/components/logo.jsx
+++ frontend/src/components/logo.jsx
@@ -3,8 +3,9 @@ function Logo() {
     return(
         <>
             <div className="logo-main">
-                <h2><span>i</span>News.</h2>
+                <h2><span className="text-red-700" >i</span>News.</h2>
             </div>
         </>
     )
 }
+export default Logo;
