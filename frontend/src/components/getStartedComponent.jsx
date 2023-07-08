3e47283bd476fe4750f7492ef62a9cb13d1c6c77
jegathees02 <100774735+jegathees02@users.noreply.github.com>
2023-07-08 11:36:05 +0530

    day 01

---
+++ frontend/src/components/getStartedComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function getStartedComponent() {
+    return(
+        
+            <div >
+                <h1>Hello</h1>
+                <h5>This is News aggregator</h5>
+
+            </div>
+        
+    )
+}
+export default getStartedComponent;
