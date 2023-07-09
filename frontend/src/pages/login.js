6bcf4398ec5409620c92d21b52e00e3b4d806336
jegathees02 <100774735+jegathees02@users.noreply.github.com>
2023-07-09 19:47:23 +0530

    login_updated

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -9,7 +9,7 @@ function Login() {
                 <Logo/>
             </div>
             <div className="login-content row">
-                 <div className="login-img col-md-12 col-sm-2 col-lg-12"  >
+                 <div className="login-img col-md-6 col-sm-12 col-lg-6"  >
                     <div>
                         <img className="login-img-main" src="https://c1.wallpaperflare.com/preview/415/175/739/magnifier-newspaper-history-glass.jpg" alt="img" />
                     </div>
@@ -17,19 +17,25 @@ function Login() {
                     <div className="login-inputs" >
                         <div className="login-input-main" >
                             <div className="login-text">
-                                <h2 className="text-slate-50 text-2xl font-bold text-center">iNews. Login</h2>
+                                <h2 className="text-slate-50 text-4xl font-bold  text-center"><span className="text-red-500">i</span>News<span className="text-red-500">.</span> &nbsp; Login</h2>
                             </div>
                             <div className="login-input-form">
-                                <form>
-                                    <label className="text-slate-100">Username/Email:
-                                        <input type="text" placeholder="Email/Username" />
-                                    </label>
-                                    <label className="text-slate-100">Password:
-                                        <input type="password" placeholder="Password"></input>
-                                    </label>
-                                    <label className="text-slate-100">
+                                <form className="form-group">
+                                    <label className="text-slate-100 text-3xl login-input">Username/Email:
+                                        <input className="input-box text-slate-950" type="text" placeholder="Email/Username" />
+                                        </label>
+                                    <label className="text-slate-100 text-3xl login-input">Password:
+                                        <input className="input-box text-slate-950" type="password" placeholder="Password"></input>
+                                        </label>
+                                        <div className="login-button">
+                                        <button className="login-clear-button text-slate-100">Clear</button>
+                                        <button className="login-login-button  text-slate-100">Login</button>
+                                        </div>
+                                        
+                                    <label className="text-slate-100 text-3xl login-input">
                                         New User??
-                                        <a href="/signup" >SignUp</a>
+                                        {/* <a href="/signup" >SignUp</a> */}
+                                        <button className="login-signup-button text-slate-100">SignUp</button>
                                         {/* <Link to "/signup">Sign Up </Link> */}
                                     </label>
                                 </form>
